13916b2fcb387e0f4491f64fdf1b806886f19402
JuneYoung <nietsnie1@gmail.com>
2024-12-26 09:17:45 +0900

    feat: add tooltips and alias for functions

--- canonical_transformer/__init__.py
+++ canonical_transformer/__init__.py
@@ -1,6 +1,6 @@
 from .dataframe_utils import *
 from .save_utils import *
-from .mapping_alias import *
+from .alias_functions import *
 from .specialized_utils import *
 from .format_utils import *
 from .loader_utils import *
